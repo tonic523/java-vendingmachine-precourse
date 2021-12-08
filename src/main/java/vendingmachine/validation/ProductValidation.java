6bdf36c71b7b62502cd56ec3c2f8d9b845e64d09
wooseok <diddmlvkf@gmail.com>
2021-12-08 22:28:46 +0900

    feat(ProductValidation) : 상품의 가격이 10의 배수인지 검사 구현

--- src/main/java/vendingmachine/validation/ProductValidation.java
+++ src/main/java/vendingmachine/validation/ProductValidation.java
@@ -2,9 +2,16 @@ package vendingmachine.validation;
 
 public class ProductValidation {
 	private static final int MIN_PRICE = 100;
+	private static final int MULTIPLE_STANDARD = 10;
 
 	public static void moreExpensive(int price) throws IllegalArgumentException {
 		if (price < MIN_PRICE) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public static void validateMultipleOfTen(int price) throws IllegalArgumentException {
+		if (price % MULTIPLE_STANDARD != 0) {
 			throw new IllegalArgumentException();
 		}
 	}
--- src/test/java/vendingmachine/validation/ProductValidationTest.java
+++ src/test/java/vendingmachine/validation/ProductValidationTest.java
@@ -6,10 +6,17 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class ProductValidationTest {
-	@DisplayName("가격은 100보다 커야 한다.")
+	@DisplayName("상품 가격은 100원보다 비싸다.")
 	@Test
 	void priceMinValue() {
 		assertThatThrownBy(() -> ProductValidation.moreExpensive(99))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("상품 가격은 10의 배수이다.")
+	@Test
+	void validateMultipleOfTen() {
+		assertThatThrownBy(() -> ProductValidation.validateMultipleOfTen(101))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
