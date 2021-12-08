0bb10fc19a81f87da92e6329a473894390981380
wooseok <diddmlvkf@gmail.com>
2021-12-08 21:20:25 +0900

    feat(Change) : 잔돈은 10의 배수여야 하는 검증 구현

--- src/main/java/vendingmachine/model/Change.java
+++ src/main/java/vendingmachine/model/Change.java
@@ -3,12 +3,20 @@ package vendingmachine.model;
 public class Change {
 
 	private static final int MIN_MONEY = 10;
+	private static final int MULTIPLE_STANDARD = 10;
 
 	private int money;
 
 	public Change(int money) throws IllegalArgumentException {
+		validateMultipleOfTen(money);
 		validateMinMoney(money);
 		this.money = money;
+	}
+
+	private static void validateMultipleOfTen(int money) throws IllegalArgumentException {
+		if (money % MULTIPLE_STANDARD != 0) {
+			throw new IllegalArgumentException();
+		}
 	}
 
 	private static void validateMinMoney(int money) throws IllegalArgumentException {
--- src/test/java/vendingmachine/model/ChangeTest.java
+++ src/test/java/vendingmachine/model/ChangeTest.java
@@ -9,9 +9,17 @@ import static org.assertj.core.api.Assertions.*;
 public class ChangeTest {
 	@DisplayName("잔돈은 최소 10원 이상이어야 한다.")
 	@Test
-	void validateValue() {
+	void validateMinMoney() {
 		new Change(10);
 		assertThatThrownBy(() -> new Change(9))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("잔돈은 10의 배수이다.")
+	@Test
+	void validateMultipleOfTen() {
+		new Change(10);
+		assertThatThrownBy(() -> new Change(11))
 			.isInstanceOf(IllegalArgumentException.class);
 	}
 }
